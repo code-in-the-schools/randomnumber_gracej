9472fa78d3d4cfa729c4cc48caf4e59fd9ae4eef
gracejoh <ganwynj@icloud.com>
2020-07-21 15:25:33 +0000

    .,

--- main.py
+++ main.py
@@ -5,19 +5,15 @@ for i in range(1,10):
   n = random.randint(1,6)
   randomlist.append(n)
 
-
-import random
 randomlist2 = []
 for i in range(1,100):
   n = random.randint(1,6)
-  randomlist.append(n)
+  randomlist2.append(n)
 
-
-import random
 randomlist3 = []
 for i in range(1,10000):
   n = random.randint(1,6)
-  randomlist.append(n)
+  randomlist3.append(n)
 
 
 #find percentage for 10
@@ -73,60 +69,109 @@ print("6 | " , percent6)
 
 
 
-
 #repeat for 100
 
-print("# or iterations: 10")
+print("# or iterations: 100")
 
 a = 0
-for i in range(len(randomlist)):
-  if randomlist[i] == 1:
+for i in range(len(randomlist2)):
+  if randomlist2[i] == 1:
+    a += 1
+percent1 = (a)
+
+print("1 | " , percent1)
+
+b = 0
+for i in range(len(randomlist2)):
+  if randomlist2[i] == 2:
+    b += 1
+percent2 = (b)
+
+print("2 | " , percent2)
+
+c = 0
+for i in range(len(randomlist2)):
+  if randomlist2[i] == 3:
+    c += 1
+percent3 = (c)
+
+print("3 | " , percent3)
+
+d = 0
+for i in range(len(randomlist2)):
+  if randomlist2[i] == 4:
+    d += 1
+percent4 = (d)
+
+print("4 | " , percent4)
+
+e = 0
+for i in range(len(randomlist2)):
+  if randomlist2[i] == 5:
+    e += 1
+percent5 = (e)
+
+print("5 | " , percent5)
+
+f = 0
+for i in range(len(randomlist2)):
+  if randomlist2[i] == 6:
+    f += 1
+percent6 = (f)
+
+print("6 | " , percent6)
+
+
+#repeat for 10000
+print("number of iterations: 10000")
+
+a = 0
+for i in range(len(randomlist3)):
+  if randomlist3[i] == 1:
     a += 1
 percent1 = (a/100)
 
 print("1 | " , percent1)
 
 b = 0
-for i in range(len(randomlist)):
-  if randomlist[i] == 2:
+for i in range(len(randomlist3)):
+  if randomlist3[i] == 2:
     b += 1
 percent2 = (b/100)
 
 print("2 | " , percent2)
 
 c = 0
-for i in range(len(randomlist)):
-  if randomlist[i] == 3:
+for i in range(len(randomlist3)):
+  if randomlist3[i] == 3:
     c += 1
 percent3 = (c/100)
 
 print("3 | " , percent3)
 
 d = 0
-for i in range(len(randomlist)):
-  if randomlist[i] == 4:
+for i in range(len(randomlist3)):
+  if randomlist3[i] == 4:
     d += 1
 percent4 = (d/100)
 
 print("4 | " , percent4)
 
 e = 0
-for i in range(len(randomlist)):
-  if randomlist[i] == 5:
+for i in range(len(randomlist3)):
+  if randomlist3[i] == 5:
     e += 1
 percent5 = (e/100)
 
 print("5 | " , percent5)
 
 f = 0
-for i in range(len(randomlist)):
-  if randomlist[i] == 6:
+for i in range(len(randomlist3)):
+  if randomlist3[i] == 6:
     f += 1
 percent6 = (f/100)
 
 print("6 | " , percent6)
-#repeat for 10000
-
 ###
 
 #ask for user input of any numbers 0-9
